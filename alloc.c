52cd6f08fe57ea88dba2943af41c330bc4d89e13
Kyle Harrington <harintonkairu@gmail.com>
2025-06-07 23:52:06 -0600

    minor refactor, lose the recursion for iteration instead cuz faster and safer

--- alloc.c
+++ alloc.c
@@ -9,36 +9,36 @@ header *findblock_(
 {
   // 1 for the header and 1 for at least one word alloc
   const uint8_t RESERVED = 2;
-  if ((currWordPos + numWordsToAllocate) >
-      (MAX_WORDS - RESERVED))
+  while (1)
   {
-    reterr(ERR_NO_MEM);
-  }
+    if ((currWordPos + numWordsToAllocate) > (MAX_WORDS - RESERVED))
+    {
+      reterr(ERR_NO_MEM);
+    }
 
-  bool ok = false;
-  if (!(currHeader->w))
-  {
-    // We are at the next available block and
-    // we have already checked for enough space above
-    ok = true;
-  }
-  if (!(currHeader->allocated) &&
-      (currHeader->w >= numWordsToAllocate))
-  {
-    // We are at a block that was once allocated,
-    // and since de-allocated, and the hole is big enough
-    ok = true;
-  }
+    bool ok = false;
+    if (!(currHeader->w))
+    {
+      // We are at the next available block and
+      // we have already checked for enough space above
+      ok = true;
+    }
+    if (!(currHeader->allocated) && (currHeader->w >= numWordsToAllocate))
+    {
+      // We are at a block that was once allocated,
+      // and since de-allocated, and the hole is big enough
+      ok = true;
+    }
 
-  if (ok)
-  {
-    return currHeader;
-  }
+    if (ok)
+    {
+      return currHeader;
+    }
 
-  // Otherwise we need to continue our search for a suitable block...
-  header *nextHeader = (header *)((char *)currHeader + 4 + currHeader->w * 4);
-  uint32_t nextWordPos = currWordPos + currHeader->w;
-  return findblock_(nextHeader, numWordsToAllocate, nextWordPos);
+    // Otherwise we need to continue our search for a suitable block...
+    currHeader = (header *)((char *)currHeader + 4 + currHeader->w * 4);
+    currWordPos += currHeader->w;
+  }
 }
 
 void *mkalloc(uint32_t numWords, header *hdr)
@@ -85,9 +85,9 @@ void prettyprint_(header *hdr)
 {
   header *currHeader = hdr;
   int32_t n = 1;
-  const int32_t max_blocks = 1000000; // safety limit
+  const int32_t MAX_BLOCKS = 1000000; // safety limit
 
-  while (currHeader->w && n <= max_blocks)
+  while (currHeader->w && n <= MAX_BLOCKS)
   {
     printf("Alloc %d = %u %s words\n",
            n,
@@ -97,7 +97,7 @@ void prettyprint_(header *hdr)
     currHeader = (header *)((char *)currHeader + sizeof(header) + currHeader->w * 4);
     n++;
   }
-  if (n > max_blocks)
+  if (n > MAX_BLOCKS)
   {
     printf("...possible heap corruption or too many blocks\n");
   }
@@ -111,10 +111,22 @@ int main(int argc, char *argv[])
   void *b = alloc(2000);
   void *c = alloc(1);
 
+  // Should be +4 from base
+  // This +4 is the header for `a`
   printf("%p (alloc a)\n", a);
+
+  // +12 from `a`
+  // 8 bytes (2 words) for `a`'s allocation
+  //    7 bytes got rounded up to 8 bytes (nearest word)
+  // 4 bytes for `b`'s header
   printf("%p (alloc b)\n", b);
+
+  // +2004 from `b`
+  // 2000 bytes (500 words) for `b`'s allocation
+  // 4 bytes for `c`'s header
   printf("%p (alloc c)\n", c);
 
+  // One day I'll make proper unit tests :)
   prettyprint();
 
   return 0;
